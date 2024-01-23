214d5d3ed842076d302e126dc6a3b52f15e8c386
Chris Bourgeonnier <chrisbourgeonnier@gmail.com>
2024-01-23 08:43:44 +0700

    completed easy 136

--- easy_136_single_number.rb
+++ easy_136_single_number.rb
@@ -1,7 +1,11 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def single_number(nums)
-
+  nums.each do |num|
+    if nums.count(num) == 1
+      return num
+    end
+  end
 end
 
-single_number([1, 3, 5, 3, 1, 6, 8, 8])
+single_number([1, 3, 5, 3, 1])
